c3af8a131cde64473ab92cbdcfb6f12ab8fabe43
enda adittia <223443009@mhs.polman-bandung.ac.id>
2024-02-17 23:49:49 +0700

    menambahkan foto, memperbaiki navbar script

---
+++ src/script.js
@@ -0,0 +1,17 @@
+// navbar fixed
+window.onscroll = function(){
+    const header = document.querySelector('nav');
+    const fixednav = header.offsetTop;
+
+    if(window.pageYOffset > fixednav){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// hamburger
+const hamburger = document.querySelector('#hamburger');
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+});
