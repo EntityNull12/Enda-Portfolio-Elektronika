6d885789a863c447954f4dc6714b4d51a1a4bf13
enda adittia <223443009@mhs.polman-bandung.ac.id>
2024-02-25 13:11:42 +0700

    perbaikan folder/file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}",
+  content: ["./**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
